5a5c640184f99346807a889590cd014cf8d8764f
DanCpp <danila.ushakov.08@mail.ru>
2025-10-25 09:48:21 +0700

    re-do parser logic

--- src/app/parser.py
+++ src/app/parser.py
@@ -10,29 +10,30 @@ def calculate_KPI(diff: dict[Commit, str], people: list[str], commit_by_name: di
     for author_name in people:
         people_commits = commit_by_name[author_name]
         metric = 0
+        last_date = None
         for people_commit in people_commits:
             str_diff = diff[people_commit]
             cnt_add, cnt_del = 0, 0
 
+
             last_dura = datetime.timedelta(seconds=0)
+
+            if last_date:
+                last_dura = last_date - people_commit.commit_date
+            else:
+                last_dura = datetime.timedelta(seconds=60*60*24)
+                last_date = people_commit.commit_date
+
             for changes_str in str_diff.split('\n'):
-                if last_dura.total_seconds() != 0:
-                    last_dura = last_date - people_commit.commit_date
-                else:
-                    last_date = people_commit.commit_date
-                    last_dura = datetime.timedelta(seconds=1)
-                    continue
-                
                 if changes_str and changes_str[0] == "+" and changes_str[:3] != "+++":
                     cnt_add += 1
                 elif changes_str and changes_str[0] == '-' and changes_str[:3] != "---":
                     cnt_del += 1
 
-                metric = ((cnt_add + cnt_del * 0.25) * 10 / (math.log(last_dura.total_seconds() / 60 + 1, 2.71) + 1)) / 12
+            metric = 10 * (math.log(abs(cnt_add - cnt_del * 0.5) + 1) / math.log10(last_dura.total_seconds() / 60 + 1)) ** 0.5
 
-                if author_name not in sum_metric.keys():
-                    sum_metric[author_name] = 0
-                sum_metric[author_name] += metric
+            sum_metric[author_name] = metric
+            last_date = people_commit.commit_date
 
-                last_date = people_commit.commit_date
+
     return sum_metric
